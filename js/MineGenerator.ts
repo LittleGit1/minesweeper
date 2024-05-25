cfbe534de98f4ef24bf2837b08acd4537de60831
Bruce Jones <bvajones@icloud.com>
2024-05-25 14:31:18 +0100

    Mine generator utility class

---
+++ js/MineGenerator.ts
@@ -0,0 +1,48 @@
+import { BoardSettings } from "./types/Board";
+export class MineGenerator {
+  boardSettings: BoardSettings;
+  maxMines: number;
+  minesPlaced: number = 0;
+  iterationCount: number = 0;
+  gridCellCount: number;
+  mineMap: Array<Array<number>>;
+
+  constructor(boardSettings: BoardSettings) {
+    this.boardSettings = boardSettings;
+    this.maxMines = boardSettings.boardMines; // Total mines allowed for the difficulty level
+    this.minesPlaced = 0; // Count of mines already generated
+    this.iterationCount = 0; // Number of grid cells already iterated
+    this.gridCellCount = boardSettings.rows * boardSettings.columns; // Total number of cells in the grid
+    this.fillMineArray();
+  }
+
+  fillMineArray() {
+    this.mineMap = Array.from({ length: this.boardSettings.rows }, () =>
+      Array.from({ length: this.boardSettings.columns }, () => this.generateMine())
+    );
+  }
+
+  getIndexValue(x: number, y: number): number {
+    return this.mineMap[y][x];
+  }
+
+  generateMine(): number {
+    // If we've already reached the max number of mines, return 0 (not a mine)
+    if (this.minesPlaced >= this.maxMines) {
+      return 0;
+    }
+
+    // Calculate probability of getting a mine
+    const isMine =
+      Math.random() <
+      (this.maxMines - this.minesPlaced) /
+        (this.gridCellCount - this.iterationCount);
+
+    this.iterationCount++;
+
+    if (!isMine) return 0;
+
+    this.minesPlaced++;
+    return 1;
+  }
+}
---
+++ js/types/Board.ts
@@ -0,0 +1,5 @@
+export type BoardSettings = {
+    columns: number; // The number of columns in board the grid (x axis)
+    rows: number; // The number of rows in the board grid (y axis)
+    boardMines: number; // The number of mines to be generated 
+}
